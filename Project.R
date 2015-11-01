b1f031a22b229c6f3c5fc5086f126650ffe4fc23
Martin Rosenfeld <mrosenf4@hawk.iit.edu>
2015-11-01 14:43:25 -0600

    read data

--- Project.R
+++ Project.R
@@ -1,1 +1,6 @@
-transaction_data<-read.csv('D:/IIT/math-474/dunnhumby - The Complete Journey CSV/transaction_data.csv.csv',header=T, sep=",", fill = TRUE)
+#read data
+transaction_data<-read.csv('D:/IIT/math-474/dunnhumby - The Complete Journey CSV/transaction_data.csv',header=T, sep=",", fill = TRUE)
+product_data<-read.csv('D:/IIT/math-474/dunnhumby - The Complete Journey CSV/product.csv',header=T, sep=",", fill = TRUE)
+
+#merge data, just an example
+merged<-merge(transaction_data[c("BASKET_ID","PRODUCT_ID","QUANTITY","TRANS_TIME")],product_data[c("PRODUCT_ID","COMMODITY_DESC","DEPARTMENT")])
